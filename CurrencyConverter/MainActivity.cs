0faf2a250570b3f7260842484f59b2a9b2d9b205
André Ferreira <contato@andreferreira.me>
2017-03-10 09:03:21 -0300

    Fixed layout

--- CurrencyConverter/MainActivity.cs
+++ CurrencyConverter/MainActivity.cs
@@ -10,9 +10,27 @@ namespace CurrencyConverter
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            Button btnConvert = FindViewById<Button>(Resource.Id.btnCalculate);
+            //EditText txtDolares = FindViewById<EditText>(Resource.Id.txtDolares);
+            //EditText txtPesos = FindViewById<EditText>(Resource.Id.txtPesos);
+
+            double pesos, dolares;
+
+            btnConvert.Click += delegate
+            {
+                try
+                {
+                    //dolares = double.Parse(txtDolares.Text);
+                    //pesos = dolares * 19.5;
+                    //txtPesos.Text = pesos.ToString();
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+                }
+            };
         }
     }
 }
--- CurrencyConverter/Resources/Resource.Designer.cs
+++ CurrencyConverter/Resources/Resource.Designer.cs
@@ -57,6 +57,49 @@ namespace CurrencyConverter
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050009
+			public const int btnCalculate = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int labelEgresosC = 2131034119;
+			
+			// aapt resource value: 0x7f050003
+			public const int labelEgresosM = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int labelIngresosC = 2131034117;
+			
+			// aapt resource value: 0x7f050001
+			public const int labelIngresosM = 2131034113;
+			
+			// aapt resource value: 0x7f050008
+			public const int txtEgresosC = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int txtEgresosM = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int txtIngresosC = 2131034118;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtIngresosM = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int txtNameApp = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
